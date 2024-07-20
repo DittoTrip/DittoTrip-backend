371278663431959c95f0cfba691c1b58431dd89b
jjoonior <jjoonior97@gmail.com>
2024-07-20 21:36:23 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/site/dittotrip/ditto_trip/DittoTripApplication.java
@@ -0,0 +1,13 @@
+package site.dittotrip.ditto_trip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DittoTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DittoTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/dittotrip/ditto_trip/DittoTripApplicationTests.java
@@ -0,0 +1,13 @@
+package site.dittotrip.ditto_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DittoTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
