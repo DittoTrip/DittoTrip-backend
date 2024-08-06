f69c6035ccf850cfdf44fbf9742a064f96b502d9
1sunJ <won9619v@naver.com>
2024-08-06 22:40:20 +0900

    Fix: CategoryData categoryId 필드 추가

--- src/main/java/site/dittotrip/ditto_trip/category/domain/dto/CategoryData.java
+++ src/main/java/site/dittotrip/ditto_trip/category/domain/dto/CategoryData.java
@@ -1,17 +1,24 @@
 package site.dittotrip.ditto_trip.category.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import site.dittotrip.ditto_trip.category.domain.Category;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class CategoryData {
+
+    private Long categoryId;
     private String categoryName;
     // 카테고리 이미지
 
     public static CategoryData fromEntity(Category category) {
-        return new CategoryData(category.getCategoryName());
+
+        return CategoryData.builder()
+                .categoryId(category.getId())
+                .categoryName(category.getCategoryName()).build();
     }
 
 }
