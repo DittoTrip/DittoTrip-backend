d1c4a5d618bdb78e817fd6017c234441815ccaa7
1sunJ <won9619v@naver.com>
2024-08-06 22:28:32 +0900

    Feat: Category Detail 응답 Dto 추가

---
+++ src/main/java/site/dittotrip/ditto_trip/category/domain/dto/CategoryDetailRes.java
@@ -0,0 +1,11 @@
+package site.dittotrip.ditto_trip.category.domain.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDetailRes {
+
+    private CategoryDataInList categoryDataInList;
+    // Spot Dto 리스트
+
+}
