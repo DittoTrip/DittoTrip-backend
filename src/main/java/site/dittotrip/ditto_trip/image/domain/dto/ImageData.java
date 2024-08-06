fd13a389d6ffd5ee866938ec71c214bcea3a0816
1sunJ <won9619v@naver.com>
2024-08-07 02:59:04 +0900

    Feat: ImageData fromEntity 메서드 추가

--- src/main/java/site/dittotrip/ditto_trip/image/domain/dto/ImageData.java
+++ src/main/java/site/dittotrip/ditto_trip/image/domain/dto/ImageData.java
@@ -1,10 +1,18 @@
 package site.dittotrip.ditto_trip.image.domain.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import site.dittotrip.ditto_trip.image.domain.Image;
 
 @Data
+@Builder
 public class ImageData {
 
     private String filePath;
+
+    public static ImageData fromEntity(Image image) {
+        return ImageData.builder()
+                .filePath(image.getFilePath()).build();
+    }
 
 }
