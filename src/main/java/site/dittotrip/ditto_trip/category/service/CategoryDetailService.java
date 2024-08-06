fe12680fb9c3d89880145c6b2902e96fadad9338
1sunJ <won9619v@naver.com>
2024-08-07 03:32:10 +0900

    Feat: CategoryDetailService 주요 메서드 추가

--- src/main/java/site/dittotrip/ditto_trip/category/service/CategoryDetailService.java
+++ src/main/java/site/dittotrip/ditto_trip/category/service/CategoryDetailService.java
@@ -3,8 +3,16 @@ package site.dittotrip.ditto_trip.category.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.dittotrip.ditto_trip.category.domain.Category;
 import site.dittotrip.ditto_trip.category.domain.dto.detail.CategoryDetailRes;
 import site.dittotrip.ditto_trip.category.repository.CategoryRepository;
+import site.dittotrip.ditto_trip.spot.categoryspot.domain.CategorySpot;
+import site.dittotrip.ditto_trip.spot.categoryspot.repository.CategorySpotRepository;
+import site.dittotrip.ditto_trip.spot.domain.Spot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
@@ -12,9 +20,25 @@ import site.dittotrip.ditto_trip.category.repository.CategoryRepository;
 public class CategoryDetailService {
 
     private final CategoryRepository categoryRepository;
+    private final CategorySpotRepository categorySpotRepository;
 
+    /**
+     * 미완성
+     * - findByScope 메서드 수정
+     */
     public CategoryDetailRes findCategoryDetail(Long categoryId) {
-        return null;
+        Category category = categoryRepository.findById(categoryId).orElseThrow(NoSuchElementException::new);
+        List<CategorySpot> categorySpots = categorySpotRepository.findByScope();
+
+        return CategoryDetailRes.fromEntity(category, convertSpotList(categorySpots));
+    }
+
+    private List<Spot> convertSpotList(List<CategorySpot> categorySpots) {
+        List<Spot> spots = new ArrayList<>();
+        for (CategorySpot categorySpot : categorySpots) {
+            spots.add(categorySpot.getSpot());
+        }
+        return spots;
     }
 
 }
