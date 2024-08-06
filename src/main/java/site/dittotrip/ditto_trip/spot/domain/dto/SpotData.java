e3b96067583d0ec05d269728f628a6a857e71667
1sunJ <won9619v@naver.com>
2024-08-07 03:16:01 +0900

    Feat: SpotData fromEntity 메서드 추가

--- src/main/java/site/dittotrip/ditto_trip/spot/domain/dto/SpotData.java
+++ src/main/java/site/dittotrip/ditto_trip/spot/domain/dto/SpotData.java
@@ -1,12 +1,17 @@
 package site.dittotrip.ditto_trip.spot.domain.dto;
 
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.data.geo.Point;
 import site.dittotrip.ditto_trip.hashtag.domain.entity.Hashtag;
+import site.dittotrip.ditto_trip.spot.domain.Spot;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@Builder
 public class SpotData {
 
     private Long spotId;
@@ -16,5 +21,14 @@ public class SpotData {
     private String address;
     private Point point;
     private List<Hashtag> hashtags = new ArrayList<>();
+
+    /**
+     * 미완성
+     */
+    public static SpotData fromEntity(Spot spot) {
+        return SpotData.builder()
+                .spotId(spot.getId())
+                .build();
+    }
 
 }
