292f0bf3ae3df45f2fe6351520de8a462c8fc88d
1sunJ <won9619v@naver.com>
2024-08-06 22:32:19 +0900

    Style: Category Dto 패키지 수정

--- src/main/java/site/dittotrip/ditto_trip/category/domain/dto/detail/CategoryDetailRes.java
+++ src/main/java/site/dittotrip/ditto_trip/category/domain/dto/detail/CategoryDetailRes.java
@@ -1,6 +1,7 @@
-package site.dittotrip.ditto_trip.category.domain.dto;
+package site.dittotrip.ditto_trip.category.domain.dto.detail;
 
 import lombok.Data;
+import site.dittotrip.ditto_trip.category.domain.dto.CategoryData;
 
 @Data
 public class CategoryDetailRes {
--- src/main/java/site/dittotrip/ditto_trip/category/service/CategoryDetailService.java
+++ src/main/java/site/dittotrip/ditto_trip/category/service/CategoryDetailService.java
@@ -3,7 +3,7 @@ package site.dittotrip.ditto_trip.category.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import site.dittotrip.ditto_trip.category.domain.dto.CategoryDetailRes;
+import site.dittotrip.ditto_trip.category.domain.dto.detail.CategoryDetailRes;
 import site.dittotrip.ditto_trip.category.repository.CategoryRepository;
 
 @Service
