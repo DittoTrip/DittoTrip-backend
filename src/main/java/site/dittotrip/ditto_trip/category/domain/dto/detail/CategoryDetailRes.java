204b46e6a82b26f9fa4bc878b2ab68b51f1d5124
1sunJ <won9619v@naver.com>
2024-08-06 22:41:54 +0900

    Feat: CategoryDetailRes SpotData 리스트 필드 추가

--- src/main/java/site/dittotrip/ditto_trip/category/domain/dto/detail/CategoryDetailRes.java
+++ src/main/java/site/dittotrip/ditto_trip/category/domain/dto/detail/CategoryDetailRes.java
@@ -2,11 +2,14 @@ package site.dittotrip.ditto_trip.category.domain.dto.detail;
 
 import lombok.Data;
 import site.dittotrip.ditto_trip.category.domain.dto.CategoryData;
+import site.dittotrip.ditto_trip.spot.domain.dto.SpotData;
+
+import java.util.List;
 
 @Data
 public class CategoryDetailRes {
 
     private CategoryData categoryData;
-    // Spot Dto 리스트
+    private List<SpotData> spotData;
 
 }
