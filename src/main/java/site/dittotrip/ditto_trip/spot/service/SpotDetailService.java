d1ffd5f4383e860b5f6521991039d5603a765c82
haus <won9619v@naver.com>
2024-08-08 14:20:30 +0900

    Feat: SpotDetail 조회 기능 SpotDitto 데이터 추가

--- src/main/java/site/dittotrip/ditto_trip/spot/service/SpotDetailService.java
+++ src/main/java/site/dittotrip/ditto_trip/spot/service/SpotDetailService.java
@@ -8,8 +8,11 @@ import site.dittotrip.ditto_trip.review.repository.ReviewRepository;
 import site.dittotrip.ditto_trip.spot.domain.Spot;
 import site.dittotrip.ditto_trip.spot.domain.dto.detail.SpotDetailRes;
 import site.dittotrip.ditto_trip.spot.repository.SpotRepository;
+import site.dittotrip.ditto_trip.spot.spotditto.domain.SpotDitto;
+import site.dittotrip.ditto_trip.spot.spotditto.repository.SpotDittoRepository;
 import site.dittotrip.ditto_trip.spot.stillcut.domain.StillCut;
 import site.dittotrip.ditto_trip.spot.stillcut.repository.StillCutRepository;
+import site.dittotrip.ditto_trip.user.domain.User;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -23,14 +26,16 @@ public class SpotDetailService {
     private final SpotRepository spotRepository;
     private final StillCutRepository stillCutRepository;
     private final ReviewRepository reviewRepository;
+    private final SpotDittoRepository spotDittoRepository;
 
-    public SpotDetailRes findSpotDetail(Long spotId) {
+    public SpotDetailRes findSpotDetail(Long spotId, User user) {
         Spot spot = spotRepository.findById(spotId).orElseThrow(NoSuchElementException::new);
 
         List<StillCut> stillCuts = stillCutRepository.findTop3BySpotOrderByCreatedDateTimeDesc(spot);
         List<Review> reviews = reviewRepository.findTop3BySpotOrderByCreatedDateTimeDesc(spot);
+        SpotDitto spotDitto = spotDittoRepository.findBySpotAndUser(spot, user).orElse(null);
 
-        return SpotDetailRes.fromEntity(spot, stillCuts, reviews);
+        return SpotDetailRes.fromEntity(spot, stillCuts, reviews, spotDitto);
     }
 
 }
