7ac868c9c79e83ef177896bbd2356e4ce9255148
haus <won9619v@naver.com>
2024-08-08 14:12:03 +0900

    Feat: Spot 컨트롤러 categoryDetail 추가

--- src/main/java/site/dittotrip/ditto_trip/spot/controller/SpotController.java
+++ src/main/java/site/dittotrip/ditto_trip/spot/controller/SpotController.java
@@ -5,16 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import site.dittotrip.ditto_trip.spot.domain.dto.detail.SpotDetailRes;
+import site.dittotrip.ditto_trip.spot.service.SpotDetailService;
 
 @RestController
 @RequestMapping("/spot")
 @RequiredArgsConstructor
 public class SpotController {
 
+    private final SpotDetailService spotDetailService;
 
     @GetMapping("/{spotId}")
-    private void categoryDetail(@PathVariable(name = "spotId") Long spotId) {
-
+    private SpotDetailRes categoryDetail(@PathVariable(name = "spotId") Long spotId) {
+        return spotDetailService.findSpotDetail(spotId);
     }
 
 }
