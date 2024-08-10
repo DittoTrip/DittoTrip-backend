abebfa2af4bc37078703595c773db06fd7d0407f
haus <won9619v@naver.com>
2024-08-10 21:43:08 +0900

    Fix: CommentListRes 이전 생성자 메서드 삭제

--- src/main/java/site/dittotrip/ditto_trip/review/comment/domain/dto/list/CommentListRes.java
+++ src/main/java/site/dittotrip/ditto_trip/review/comment/domain/dto/list/CommentListRes.java
@@ -14,13 +14,4 @@ public class CommentListRes {
 
     private List<CommentData> commentDataList = new ArrayList<>();
 
-    public static CommentListRes fromEntity(List<Comment> comments) {
-        CommentListRes commentListRes = new CommentListRes();
-        for (Comment comment : comments) {
-            commentListRes.getCommentDataList().add(CommentData.parentFromEntity(comment));
-        }
-
-        return commentListRes;
-    }
-
 }
