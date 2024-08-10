2b7954f57731c1a16ad0c711988a3e245f74e325
haus <won9619v@naver.com>
2024-08-10 21:42:21 +0900

    Fix: CommentData isMine 필드 추가 및 isMine 채우는 로직 추가

--- src/main/java/site/dittotrip/ditto_trip/review/comment/domain/dto/list/CommentListRes.java
+++ src/main/java/site/dittotrip/ditto_trip/review/comment/domain/dto/list/CommentListRes.java
@@ -1,14 +1,15 @@
 package site.dittotrip.ditto_trip.review.comment.domain.dto.list;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import site.dittotrip.ditto_trip.review.comment.domain.Comment;
 import site.dittotrip.ditto_trip.review.comment.domain.dto.CommentData;
-import site.dittotrip.ditto_trip.user.domain.dto.UserData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class CommentListRes {
 
     private List<CommentData> commentDataList = new ArrayList<>();
--- src/main/java/site/dittotrip/ditto_trip/review/comment/service/CommentListService.java
+++ src/main/java/site/dittotrip/ditto_trip/review/comment/service/CommentListService.java
@@ -4,14 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.dittotrip.ditto_trip.review.comment.domain.Comment;
+import site.dittotrip.ditto_trip.review.comment.domain.dto.CommentData;
 import site.dittotrip.ditto_trip.review.comment.domain.dto.list.CommentListRes;
 import site.dittotrip.ditto_trip.review.comment.repository.CommentRepository;
 import site.dittotrip.ditto_trip.review.domain.Review;
 import site.dittotrip.ditto_trip.review.repository.ReviewRepository;
+import site.dittotrip.ditto_trip.user.domain.User;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
@@ -26,12 +29,32 @@ public class CommentListService {
      * 자식 댓글 날짜 정렬안되면 수정하기
      * 삭제된 댓글의 자식댓글 보여줄 것인가 등등 기획 확인 후 수정
      */
-    public CommentListRes findCommentList(Long reviewId) {
+    public CommentListRes findCommentList(Long reviewId, User user) {
         Review review = reviewRepository.findById(reviewId).orElseThrow(NoSuchElementException::new);
 
         List<Comment> parentComments = commentRepository.findByReviewAndParentCommentIsNullOrderByCreatedDateTimeAsc(review);
 
-        return CommentListRes.fromEntity(parentComments);
+        List<CommentData> commentDataList = new ArrayList<>();
+        for (Comment parentComment : parentComments) {
+            CommentData commentData = CommentData.parentFromEntity(parentComment);
+            commentData.setIsMine(getIsMine(user));
+            commentDataList.add(commentData);
+        }
+
+        return new CommentListRes(commentDataList);
+    }
+
+    private Boolean getIsMine(User user) {
+        if (user == null) {
+            return Boolean.FALSE;
+        }
+
+        Optional<Comment> findComment = commentRepository.findByUser(user);
+        if (findComment.isPresent()) {
+            return Boolean.TRUE;
+        } else {
+            return Boolean.TRUE;
+        }
     }
 
 }
