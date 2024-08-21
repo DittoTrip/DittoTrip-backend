31c4e0c04b6b0cfa12915a0dfdc307357bbbfa6f
haus <won9619v@naver.com>
2024-08-21 20:19:23 +0900

    Fix: SpotImageRepository ReviewImageRepository 수정

--- src/main/java/site/dittotrip/ditto_trip/spot/repository/SpotImageRepository.java
+++ src/main/java/site/dittotrip/ditto_trip/spot/repository/SpotImageRepository.java
@@ -3,5 +3,5 @@ package site.dittotrip.ditto_trip.spot.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.dittotrip.ditto_trip.spot.domain.SpotImage;
 
-public interface SpotImageRepository extends JpaRepository<Long, SpotImage> {
+public interface SpotImageRepository extends JpaRepository<SpotImage, Long> {
 }
