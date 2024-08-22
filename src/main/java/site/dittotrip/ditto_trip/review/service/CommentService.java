32816dcac4f59699698c7e9010da799d60b7cd05
haus <won9619v@naver.com>
2024-08-22 18:57:42 +0900

    Feat: ReviewComment 댓글 등롥, 수정, 삭제 컨트롤러 추가

--- src/main/java/site/dittotrip/ditto_trip/review/service/CommentService.java
+++ src/main/java/site/dittotrip/ditto_trip/review/service/CommentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.dittotrip.ditto_trip.review.domain.ReviewComment;
 import site.dittotrip.ditto_trip.review.domain.dto.CommentSaveReq;
+import site.dittotrip.ditto_trip.review.exception.NotMatchedRelationException;
 import site.dittotrip.ditto_trip.review.repository.ReviewCommentRepository;
 import site.dittotrip.ditto_trip.review.domain.Review;
 import site.dittotrip.ditto_trip.review.repository.ReviewRepository;
@@ -21,26 +22,6 @@ public class CommentService {
     private final ReviewRepository reviewRepository;
     private final ReviewCommentRepository reviewCommentRepository;
 
-    /**
-     * 등록순, 최신순 기획 변경될 수 있음
-     * 자식 댓글 날짜 정렬안되면 수정하기
-     * 삭제된 댓글의 자식댓글 보여줄 것인가 등등 기획 확인 후 수정
-     */
-//    public CommentListRes findCommentList(Long reviewId, User user) {
-//        Review review = reviewRepository.findById(reviewId).orElseThrow(NoSuchElementException::new);
-//
-//        List<ReviewComment> parentReviewComments = commentRepository.findByReviewAndParentCommentIsNullOrderByCreatedDateTimeAsc(review);
-//
-//        List<CommentData> commentDataList = new ArrayList<>();
-//        for (ReviewComment parentReviewComment : parentReviewComments) {
-//            CommentData commentData = CommentData.parentFromEntity(parentReviewComment);
-//            commentData.setIsMine(getIsMine(user));
-//            commentDataList.add(commentData);
-//        }
-//
-//        return new CommentListRes(commentDataList);
-//    }
-
     @Transactional(readOnly = false)
     public void saveComment(Long reviewId, Long parentCommentId, User user,
                             CommentSaveReq commentSaveReq) {
@@ -49,6 +30,9 @@ public class CommentService {
         ReviewComment parentReviewComment = null;
         if (parentCommentId != null) {
             parentReviewComment = reviewCommentRepository.findById(parentCommentId).orElseThrow(NoSuchElementException::new);
+            if (!parentReviewComment.getReview().equals(review)) {
+                throw new NotMatchedRelationException();
+            }
         }
 
         ReviewComment reviewComment = new ReviewComment(commentSaveReq.getBody(), user, review, parentReviewComment);
