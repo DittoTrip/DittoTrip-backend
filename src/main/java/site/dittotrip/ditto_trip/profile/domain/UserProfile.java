056605cfd1405318247952d09db0ccdf533ece8b
haus <won9619v@naver.com>
2024-08-26 06:53:23 +0900

    Feat: UserProfile 수정 기능 추가 및 UserData 클래스 추가

--- src/main/java/site/dittotrip/ditto_trip/profile/domain/UserProfile.java
+++ src/main/java/site/dittotrip/ditto_trip/profile/domain/UserProfile.java
@@ -3,6 +3,7 @@ package site.dittotrip.ditto_trip.profile.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import site.dittotrip.ditto_trip.item.domain.Badge;
 import site.dittotrip.ditto_trip.item.domain.Item;
 import site.dittotrip.ditto_trip.user.domain.User;
@@ -26,26 +27,32 @@ public class UserProfile {
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "item_skin_id")
+    @Setter
     private Item itemSkin;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "item_eyes_id")
+    @Setter
     private Item itemEyes;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "item_mouse_id")
+    @Setter
     private Item itemMouse;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "item_hair_id")
+    @Setter
     private Item itemHair;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "item_accessory_id")
+    @Setter
     private Item itemAccessory;
 
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "user_badge_id")
+    @Setter
     private Badge badge;
 
 }
--- src/main/java/site/dittotrip/ditto_trip/profile/repository/UserProfileRepository.java
+++ src/main/java/site/dittotrip/ditto_trip/profile/repository/UserProfileRepository.java
@@ -2,6 +2,12 @@ package site.dittotrip.ditto_trip.profile.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.dittotrip.ditto_trip.profile.domain.UserProfile;
+import site.dittotrip.ditto_trip.user.domain.User;
+
+import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    Optional<UserProfile> findByUser(User user);
+
 }
