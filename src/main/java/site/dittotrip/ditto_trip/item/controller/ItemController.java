147238fc3df620a6a6b451b61d9a0398fdaa99ac
haus <won9619v@naver.com>
2024-08-26 07:42:11 +0900

    Fix: ItemController 누락된 애노테이션 추가

--- src/main/java/site/dittotrip/ditto_trip/item/controller/ItemController.java
+++ src/main/java/site/dittotrip/ditto_trip/item/controller/ItemController.java
@@ -1,16 +1,25 @@
 package site.dittotrip.ditto_trip.item.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import site.dittotrip.ditto_trip.item.domain.dto.UserItemListRes;
 import site.dittotrip.ditto_trip.item.service.ItemService;
 
 /**
  * 1. 유저의 보유 아이템 리스트 조회
  */
+@RestController
+@RequestMapping("/user/{userId}/item")
+@RequiredArgsConstructor
 public class ItemController {
 
-    private ItemService itemService;
+    private final ItemService itemService;
 
-    public UserItemListRes UsersItemList(Long userId) {
+    @GetMapping("/list")
+    public UserItemListRes UsersItemList(@PathVariable(name = "userId") Long userId) {
         return itemService.findUsersItemList(userId);
     }
 
