33ce39833d5f2c3e3d3f03d3d9a61031259fa7c4
haus <won9619v@naver.com>
2024-08-26 10:44:43 +0900

    Feat: Follow entity 추가

---
+++ src/main/java/site/dittotrip/ditto_trip/follow/domain/Follow.java
@@ -0,0 +1,27 @@
+package site.dittotrip.ditto_trip.follow.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.dittotrip.ditto_trip.user.domain.User;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class Follow {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "following_user_id")
+    private User Following;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "followed_user_id")
+    private User followed;
+
+}
