a53d9206a734cfa23d226ed152f28ccabaa18ea4
haus <won9619v@naver.com>
2024-08-27 16:22:21 +0900

    Feat: 임시 유저 데이터 등록 컴포넌트 추가

---
+++ src/main/java/site/dittotrip/ditto_trip/test/TestDataConfig.java
@@ -0,0 +1,44 @@
+package site.dittotrip.ditto_trip.test;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import site.dittotrip.ditto_trip.user.domain.User;
+import site.dittotrip.ditto_trip.user.repository.UserRepository;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TestDataConfig {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void testUserDataInit() {
+        log.info("===== TEST DATA INIT START =====");
+
+        String email = "won9619v@naver.com";
+        String nickname = "haus";
+        String password = "1234";
+        userRepository.findByEmail(email).ifPresent(m -> {
+            log.info("===== ALREADY DATA INIT =====");
+            return;
+        });
+
+        User user = new User();
+        user.setEmail(email);
+        user.setNickname(nickname);
+        user.setPassword(passwordEncoder.encode(password));
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+        user.getAuthorities().add(authority);
+        userRepository.save(user);
+
+        log.info("===== TEST DATA INIT END =====");
+    }
+
+}
