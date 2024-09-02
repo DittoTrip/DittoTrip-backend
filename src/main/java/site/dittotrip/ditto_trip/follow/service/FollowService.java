66934db0b607ea8f6313a004a58c43eb822a3b16
haus <won9619v@naver.com>
2024-09-02 19:46:16 +0900

    Feat: Follow 등록 시 발생하는 알림 기능 추가

--- src/main/java/site/dittotrip/ditto_trip/follow/service/FollowService.java
+++ src/main/java/site/dittotrip/ditto_trip/follow/service/FollowService.java
@@ -3,6 +3,8 @@ package site.dittotrip.ditto_trip.follow.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import site.dittotrip.ditto_trip.alarm.domain.Alarm;
+import site.dittotrip.ditto_trip.alarm.repository.AlarmRepository;
 import site.dittotrip.ditto_trip.follow.domain.Follow;
 import site.dittotrip.ditto_trip.follow.domain.dto.FollowListRes;
 import site.dittotrip.ditto_trip.follow.exception.ExistingFollowException;
@@ -12,6 +14,7 @@ import site.dittotrip.ditto_trip.review.exception.NoAuthorityException;
 import site.dittotrip.ditto_trip.user.domain.User;
 import site.dittotrip.ditto_trip.user.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -23,6 +26,7 @@ public class FollowService {
 
     private final UserRepository userRepository;
     private final FollowRepository followRepository;
+    private final AlarmRepository alarmRepository;
 
     public FollowListRes findFollowingList(User reqUser, Long userId) {
         User user = userRepository.findById(userId).orElseThrow(NoSuchElementException::new);
@@ -52,6 +56,8 @@ public class FollowService {
 
         Follow follow = new Follow(reqUser, user);
         followRepository.save(follow);
+
+        // 알림 처리
     }
 
     @Transactional(readOnly = false)
@@ -64,6 +70,14 @@ public class FollowService {
         }
 
         followRepository.delete(follow);
+    }
+
+    private void processAlarmInSaveFollow(Follow follow) {
+        String title = "새로운 팔로워가 생겼어요 !!";
+        String body = follow.getFollowingUser().getNickname() + "님이 당신을 팔로우 했습니다.";
+        String path = "/user-page/" + follow.getFollowingUser().getId(); // 수정해야 할 수 있음 !!
+        List<User> targets = new ArrayList<>(List.of(follow.getFollowedUser()));
+        alarmRepository.saveAll(Alarm.createAlarms(title, body, path, targets));
     }
 
 }
