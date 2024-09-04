00ec7a63b6ffe2d68a86eb7a67f651977f4c65ba
haus <won9619v@naver.com>
2024-09-04 15:55:39 +0900

    Fix: RewardRepository 잘못된 코드 수정

--- src/main/java/site/dittotrip/ditto_trip/reward/repository/RewardRepository.java
+++ src/main/java/site/dittotrip/ditto_trip/reward/repository/RewardRepository.java
@@ -1,6 +1,7 @@
 package site.dittotrip.ditto_trip.reward.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.dittotrip.ditto_trip.reward.domain.Reward;
 
-public interface RewardRepository extends JpaRepository<RewardRepository, Long> {
+public interface RewardRepository extends JpaRepository<Reward, Long> {
 }
