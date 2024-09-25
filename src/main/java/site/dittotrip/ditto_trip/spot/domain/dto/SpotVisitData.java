4eb987f8ff31fc49ccb296501e2b228a55cf556e
1sunJ <won9619v@naver.com>
2024-09-25 20:29:50 +0900

    Feat: SpotVisitData imagePath 및 hashtags 필드 추가

--- src/main/java/site/dittotrip/ditto_trip/spot/domain/dto/SpotVisitData.java
+++ src/main/java/site/dittotrip/ditto_trip/spot/domain/dto/SpotVisitData.java
@@ -2,11 +2,14 @@ package site.dittotrip.ditto_trip.spot.domain.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import site.dittotrip.ditto_trip.hashtag.domain.HashtagSpot;
 import site.dittotrip.ditto_trip.review.domain.dto.ReviewMiniData;
 import site.dittotrip.ditto_trip.spot.domain.Spot;
 import site.dittotrip.ditto_trip.spot.domain.SpotVisit;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
@@ -16,6 +19,9 @@ public class SpotVisitData {
     private Long spotId;
     private String spotName;
     private String address;
+    private String imagePath;
+    @Builder.Default
+    private List<String> hashtags = new ArrayList<>();
     private LocalDateTime createdDateTime;
 
     private Long bookmarkId;
@@ -28,12 +34,17 @@ public class SpotVisitData {
                 .spotId(spot.getId())
                 .spotName(spot.getName())
                 .address(spot.getAddress())
+                .imagePath(spot.getImagePath())
                 .createdDateTime(spotVisit.getCreatedDateTime())
                 .bookmarkId(bookmarkId)
                 .build();
 
         if (spotVisit.getReview() != null) {
             spotVisitData.setReviewMiniData(ReviewMiniData.fromEntity(spotVisit.getReview()));
+        }
+
+        for (HashtagSpot hashtagSpot : spot.getHashtagSpots()) {
+            spotVisitData.getHashtags().add(hashtagSpot.getHashtag().getName());
         }
 
         return spotVisitData;
